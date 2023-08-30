983931f41703e85adef421bb19ac00619ae3c97e
Nikolaos Kladis <nikcladis@gmail.com>
2023-08-30 16:22:33 +0300

    wda_v1.0.2

--- js/list.js
+++ js/list.js
@@ -15,3 +15,21 @@ function toggleFilterSidebar() {
 function updatePriceValue(val) {
   document.getElementById("priceValue").innerText = val;
 }
+
+// Get today's date in the format "YYYY-MM-DD"
+const today = new Date().toISOString().split("T")[0];
+
+// Set the minimum date for check-in to be today
+document.getElementById("check-in").min = today;
+
+// When the user selects a check-in date, update the minimum check-out date
+document.getElementById("check-in").addEventListener("change", function () {
+  const selectedDate = this.value;
+  const nextDay = new Date(
+    new Date(selectedDate).getTime() + 24 * 60 * 60 * 1000
+  )
+    .toISOString()
+    .split("T")[0];
+  document.getElementById("check-out").min = nextDay;
+  document.getElementById("check-out").value = nextDay; // Optional: set the check-out date automatically
+});
